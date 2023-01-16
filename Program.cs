fb511e5f608ee78614bf5413b6aad8a242ce38cb
alexandresevilha1 <Alexandre.sevilha1@gmail.com>
2023-01-16 11:49:13 -0300

    0.7

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
 
 namespace SistemaBancario
@@ -9,167 +10,111 @@ namespace SistemaBancario
         static void Main(string[] args)
         {
 
-                string opcao;
-                string nome;
-                string valor;            
-            
+            string opcao;
+            string nome;
+            string valor;
+
+            do
+            {
+
                 Console.WriteLine(@"
-                  ----------------------------------------
-                  |                                      |
-                  |  !!Bem-Vindo(a) ao meu Super Banco!! |
-                  |                                      |
-                  ----------------------------------------
+                  --------------------------------------
+                  |                                    |
+                  |  !!Bem vindo ao meu SUPER BANCO!!  |
+                  |                                    |
+                  --------------------------------------
+
                 ");
 
                 Console.ReadLine();
 
                 Console.Clear();
-
                 Console.WriteLine(@"
                   --------------------------------------------
                   |                                          |
-                  |  Por favor selecione a operação desejada |
+                  |  Por favor digite uma das opções abaixo  |
                   |                                          |
                   |  1 - Criar uma nova conta                |
                   |                                          |
+                  |          digite 0 para encerrar o sistema|
                   --------------------------------------------
                 ");
 
                 opcao = Console.ReadLine();
 
-                switch (opcao)
+                switch(Convert.ToInt32(opcao)) 
                 {
-                case "1":
-
+                    case 1:
                         Console.Clear();
-
                         Console.WriteLine(@"
-                        -------------------------------------------
-                        |                                         |
-                        |  Por favor digite o nome do propietario |
-                        |                                         |
-                        -------------------------------------------
+                          ----------------------------------------
+                          |                                      |
+                          |  Por favor digite o nome do Titular  |
+                          |                                      |
+                          |      digite 0 para encerrar o sistema|
+                          ----------------------------------------
                         ");
 
                         nome = Console.ReadLine();
 
-                         Console.WriteLine(@"
-                        --------------------------------------
-                        |                                    |
-                        |  Por favor digite o valor inicial  |
-                        |                                    |
-                        --------------------------------------
+                        Console.Clear();
+                        Console.WriteLine(@"
+                          --------------------------------------------------
+                          |                                                |
+                          |  Por favor digite o valor de deposito inicial  |
+                          |                                                |
+                          |                digite 0 para encerrar o sistema|
+                          --------------------------------------------------
                         ");
 
                         valor = Console.ReadLine();
 
-                        var conta = new ContaBancaria(nome, Convert.ToDecimal(valor));
-
-                        Console.Clear();
-
-                        Console.WriteLine(@$"
-                        -----------------------------------------------------------------
-                        |                                                               |
-                        |  Parabens {conta.Titular}!! sua conta foi criada com sucesso  |
-                        |                                                               |
-                        |  Numero da conta: {conta.Numero}                              |
-                        |  Saldo da conta: {conta.Balanco}                              |
-                        -----------------------------------------------------------------
-                        "); 
-
-                        Console.ReadLine();
-
-                    do
-                    {
-
-                        Console.Clear();
-
-                        Console.WriteLine(@"
-                        ------------------------------------------
-                        |                                        |
-                        |  Por favor selecione a opção desejada  |
-                        |                                        |
-                        |  1 - Fazer deposito                    |
-                        |  2 - Fazer saque                       |
-                        |  3 - Historico de transações           |
-                        |                                        |
-                        ------------------------------------------
-                        ");
-
-                        opcao = Console.ReadLine();
-
-                        switch (opcao)
+                        try
                         {
-                            case "1":
-                                Console.WriteLine(@"
-                                ---------------------------------------
-                                |                                     |
-                                |  Por favor digite o valor desejado  |
-                                |                                     |
-                                ---------------------------------------
-                                ");
+                            var conta = new ContaBancaria(nome, Convert.ToInt32(valor));
 
-                                valor = Console.ReadLine();
-
-                                conta.FazerDeposito(Convert.ToDecimal(valor), DateTime.Now, "Entrada");
-
-                                Console.WriteLine(@$"
-                                --------------------------------------------------------
-                                |                                                      |
-                                |  Pronto {conta.Titular}, Deposito feito com sucesso  |
-                                |                                                      |
-                                |  Balanço: {conta.Balanco}                            |
-                                |                                                      |
-                                --------------------------------------------------------
-                                ");
-
-                                Console.ReadLine();
-
-                                break;
-
-                            case "2":
-
-                                Console.WriteLine(@"
-                                ---------------------------------------
-                                |                                     |
-                                |  Por favor digite o valor desejado  |
-                                |                                     |
-                                ---------------------------------------
-                                ");
-
-                                valor = Console.ReadLine();
-
-                                conta.FazerSaque(Convert.ToDecimal(valor), DateTime.Now, "Saida");
-
-                                Console.WriteLine(@$"
-                                -----------------------------------------------------
-                                |                                                   |
-                                |  Pronto {conta.Titular}, Saque feito com sucesso  |
-                                |                                                   |
-                                |  Balanço: {conta.Balanco}                         |
-                                |                                                   |
-                                -----------------------------------------------------
-                                ");
-
-                                Console.ReadLine();
-
-                                break;
-
+                            Console.Clear();
+                            Console.WriteLine(@$"
+                              -------------------------------------------------------------------
+                              |                                                                 |
+                              |  Parabens {conta.Titular}!! você criou sua conta com sucesso!!  |
+                              |                                                                 |
+                              |  Numero da conta: {conta.Numero}                                |
+                              |  Balanço da conta: {conta.Balanco}                              |
+                              |                                                                 |
+                              |                                 digite 0 para encerrar o sistema|
+                              -------------------------------------------------------------------
+                            ");
+                        }
+                        catch (Exception)
+                        {
+                            Console.Clear();
+                            Console.WriteLine(@"
+                              ---------------------------------------
+                              |                                     |
+                              |  Por favor digite um valor vállido  |
+                              |                                     |
+                              |     digite 0 para encerrar o sistema|
+                              ---------------------------------------
+                            ");
                         }
 
-                default:
-                    Console.WriteLine(@"
-                    ---------------------------------------
-                    |                                     |
-                    |  Por favor digite uma opção válida  |
-                    |                                     |
-                    ---------------------------------------
-                    ");
-
-                    Console.ReadLine();
                         break;
 
-            }
+                    default:
+                        Console.Clear();
+                        Console.WriteLine(@"
+                          ---------------------------------------
+                          |                                     |
+                          |  Por favor digite uma opção válida  |
+                          |                                     |                                                
+                          ---------------------------------------
+                        ");
+                        break;
+                }
+
+
+            } while (Convert.ToInt32(opcao) != 0);
 
         }
     }   
